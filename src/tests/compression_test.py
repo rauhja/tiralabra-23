0deb33fe8f2b71759915cde765ea720fd9da521a
rauhja <82282638+rauhja@users.noreply.github.com>
2023-04-02 21:53:32 +0300

    Update test files

---
+++ src/tests/compression_test.py
@@ -0,0 +1,11 @@
+import unittest
+from services.compressservice import CompressManagement
+
+class TestCompressionService(unittest.TestCase):
+    def setUp(self):
+        self.compress = CompressManagement()
+        
+    # def test_compress_file(self):
+    #     self.compress.compress_file("test-data/test.txt", "1")
+    #     data = self.compress.filemgmt.get_compressed_file("test-data/test.huff")
+    #     self.assertEqual(data, "b'{"0": "a", "10": "c", "11": "b"}\x07\x1f\x00'")
---
+++ src/tests/fileservice_test.py
@@ -0,0 +1,20 @@
+import unittest
+from services.fileservice import FileManagementService
+
+class TestFileService(unittest.TestCase):
+    def setUp(self):
+        self.flmgmt = FileManagementService()
+        
+    def test_get_uncompressed_file(self):
+        data = self.flmgmt.get_uncompressed_file("test-data/test.txt")
+        self.assertEqual(data, "aaabbc")
+    
+    def test_create_txt_file(self):
+        self.flmgmt.create_txt_file("test-data/test.txt", "aaabbc")
+        data = self.flmgmt.get_uncompressed_file("test-data/test.txt")
+        self.assertEqual(data, "aaabbc")
+    
+    # def test_create_compressed_file(self):
+    #     self.flmgmt.create_compressed_file("test-data/test.huff", "")
+    #     data = self.flmgmt.get_compressed_file("test-data/test.huff")
+    #     self.assertEqual(data, "aaabbc")
