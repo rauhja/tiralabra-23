17f7717e86f5227f771f4074cbddce49518c047e
rauhja <82282638+rauhja@users.noreply.github.com>
2023-04-30 17:27:49 +0300

    Update tests

--- src/tests/huffman_test.py
+++ src/tests/huffman_test.py
@@ -1,10 +1,13 @@
 import unittest
+import os
 from entities.huffman import HuffmanCoding
+from services.fileservice import FileManagementService
 
 
 class TestHuffman(unittest.TestCase):
     def setUp(self):
         self.huffman = HuffmanCoding()
+        self.FM = FileManagementService()
 
     def test_calc_frequency(self):
         data = "abacabad"
@@ -30,3 +33,19 @@ class TestHuffman(unittest.TestCase):
         self.assertEqual(self.huffman.heap[0].right.right.char, None)
         self.assertEqual(self.huffman.heap[0].right.right.left.char, 'c')
         self.assertEqual(self.huffman.heap[0].right.right.right.char, 'd')
+    
+    def test_huffman_run_analysis(self):
+        data = self.FM.get_uncompressed_file("test-data/test.txt")
+        self.huffman.huffman_run_analysis(data, "test-data/test.txt")
+        result = self.FM.get_uncompressed_file("test-data/test_decomp_huf.txt")
+        self.assertEqual(data, result)
+        os.remove("test-data/test_decomp_huf.txt")
+        os.remove("test-data/test.huf")
+    
+    def test_huffman_run_analysis_large_file(self):
+        data = self.FM.get_uncompressed_file("test-data/gutenberg-top-10.txt")
+        self.huffman.huffman_run_analysis(data, "test-data/gutenberg-top-10.txt")
+        result = self.FM.get_uncompressed_file("test-data/gutenberg-top-10_decomp_huf.txt")
+        self.assertEqual(data, result)
+        os.remove("test-data/gutenberg-top-10_decomp_huf.txt")
+        os.remove("test-data/gutenberg-top-10.huf")
